3d2e7fe03c7b23507fc0b1dee43b9adc29642d7c
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-08-19 07:57:11 -0700

    i'm working to login

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,6 +2,7 @@ import userModel from '../model/user.model.js';
 import {validationResult} from 'express-validator';
 import { BadRequestError } from '../errors/BadRequestError.js';
 import { NotFoundError} from '../errors/NotFoundError.js'
+import bcrypt from 'bcrypt'
 
 
 const userController={
@@ -32,7 +33,26 @@ const userController={
     console.log(err.message)
     res.status(500).json({message:"Internal server error"})
  }
+    },
 
+    loginUser:async(req,res,next) => {
+      const {email,password}=req.body
+
+      try{
+         const findUser= await userModel.findOne({email})
+         if(!findUser){
+            return next(new Error('User not found'));
+         }
+         const isMatch=await bcrypt.compare(password,findUser.password);
+         if(!isMatch){
+            return next(new Error('Invalid credentials'));
+         }
+         res.status(200).json({message:`Hey ${findUser.names}, Welcome to our page`})
+      }
+      catch(err){
+         console.log(err.message)
+         return next(new Error('Internal server error'));
+      }
     }
 }
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,5 +3,6 @@ import userController from "../controller/user.controller.js";
 const userRouter = express.Router()
 
 userRouter.post('/create',userController.createUser)
+userRouter.get('/login',userController.loginUser)
 
 export default userRouter
