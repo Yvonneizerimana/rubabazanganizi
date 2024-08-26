6ac5a005d3814dad46a8e2a611dfb42920e6fb8d
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-08-26 06:56:55 -0700

    i have added deleteBook

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -119,7 +119,24 @@ const bookController = {
             console.log("Error updating book:", error.message);
             res.status(500).json({ message: "Internal server error" });
         }
+    },
+
+    deleteBook: async (req, res) => {
+        try{
+        const { id } = req.params;
+        const deleteBook= await bookModel.findByIdAndDelete(id);
+        if(!deleteBook){
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        res.status(200).json({ message: 'Book deleted successfully' });
+        
     }
+        catch(error) {
+            console.log("Error deleting book:", error.message);
+            res.status(500).json({ message: "Internal server error" });
+        
+    }
+}
 
 };
 
--- routes/book.route.js
+++ routes/book.route.js
@@ -7,5 +7,6 @@ routerBook.post('/addBook', bookController.uploadAndAddBook);
 routerBook.put('/updateBook/:id', bookController.updateBook);
 routerBook.get('/getOneBook/:id', bookController.getBookById);
 routerBook.get('/getAllBooks', bookController.getAllBooks);
+routerBook.delete('/deleteBook/:id', bookController.deleteBook);
 
 export default routerBook;
